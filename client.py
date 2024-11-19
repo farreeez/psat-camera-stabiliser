f3252bc74814120d3644a5c4ef0b4166e1284880
Fares <fares.selwadii@gmail.com>
2024-11-19 23:21:45 +1300

    sensor data fusion working

--- client.py
+++ client.py
@@ -14,7 +14,12 @@ HOST = "192.168.1.67"  # The server's hostname or IP address
 PORT = 65002  # The port used by the server
 
 
-def saveJson(jsonCharArr, jsonArray):
+totGyroX = 0
+totGyroY = 0
+totGyroZ = 0
+
+
+def saveJson(jsonCharArr, jsonArray, rotationArray):
     try:
         jsonString = "".join(jsonCharArr)
 
@@ -22,14 +27,6 @@ def saveJson(jsonCharArr, jsonArray):
 
         gyroData = jsonObject["gyroscope"]["value"]
         accelData = jsonObject["accelerometer"]["value"]
-
-        oldData = []
-
-        calculateAgnle = False
-
-        if jsonArray and all(jsonArray):
-            oldData = [row[-1] for row in jsonArray]
-            calculateAgnle = True
 
         improvedCamera.rotateZ(gyroData[2])
 
@@ -47,13 +44,34 @@ def saveJson(jsonCharArr, jsonArray):
 
         jsonArray[6].append(timestamp_s)
 
-        if calculateAgnle:
-            timeDiff = timestamp_s - oldData[6]
+        if jsonArray and all(jsonArray):
+            # sets weights for complementary filter
+            gyroWeight = 0.995
+            accelWeight = 1 - gyroWeight
+    
+            maxCol = len(jsonArray[0]) - 1
             
-            gyroRotX = timeDiff * (jsonArray[0][-1] + oldData[0]) / 2
-            gyroRotY = timeDiff * (jsonArray[1][-1] + oldData[1]) / 2
-            gyroRotZ = timeDiff * (jsonArray[2][-1] + oldData[2]) / 2
-
+            # gets the time taken for the measurement to occur
+            timeDiff = timestamp_s - jsonArray[6][maxCol - 1]
+            
+            # calculates the gyroscope rotation angles over that time step
+            gyroRotX = timeDiff * (jsonArray[0][maxCol] + jsonArray[0][maxCol - 1]) / 2
+            gyroRotY = timeDiff * (jsonArray[1][maxCol] + jsonArray[1][maxCol - 1]) / 2
+            gyroRotZ = timeDiff * (jsonArray[2][maxCol] + jsonArray[2][maxCol - 1]) / 2
+            
+            global totGyroX
+            global totGyroY
+            global totGyroZ
+            
+            totGyroX += gyroRotX
+            totGyroY += gyroRotY
+            totGyroZ += gyroRotZ
+            
+            # adds past rotation data onto the new step to get the total rotation over time
+            gyroRotX += improvedCamera.getXRot()
+            gyroRotY += improvedCamera.getYRot()
+            
+            # calculates the acceleration rotation angles from the accelerometer data
             accelRotX = math.atan(
                 accelData[1] / (accelData[0] ** 2 + accelData[2] ** 2)
             )
@@ -61,6 +79,31 @@ def saveJson(jsonCharArr, jsonArray):
             accelRotY = math.atan(
                 accelData[0] / (math.sqrt(accelData[1] ** 2 + accelData[2] ** 2))
             )
+            
+            # gets the weighted value of the gyro data
+            weightedGyroRotX = gyroRotX * gyroWeight
+            weightedGyroRotY = gyroRotY * gyroWeight
+            
+            # gets the weighted value of the accelerometer rotation
+            weightedAccelRotX = accelRotX * accelWeight
+            weightedAccelRotY = accelRotY * accelWeight
+            
+            # gets the total rotation by summing both values
+            totRotX = weightedAccelRotX + weightedGyroRotX
+            totRotY = weightedAccelRotY + weightedGyroRotY
+            
+            rotationArray[0].append(totGyroX)
+            rotationArray[1].append(totGyroY)
+            rotationArray[2].append(accelRotX)
+            rotationArray[3].append(accelRotY)
+            rotationArray[4].append(totRotX)
+            rotationArray[5].append(totRotY)
+            rotationArray[6].append(timestamp_ns)
+            
+            # sets the total rotation up to this step to the calculated values for it to be then used later
+            improvedCamera.rotateX(totRotX)
+            improvedCamera.rotateY(totRotY)
+            improvedCamera.rotateZ(totGyroZ)
 
     except json.JSONDecodeError:
         print("fail")
@@ -69,7 +112,7 @@ def saveJson(jsonCharArr, jsonArray):
 
 thread = threading.Thread(target=improvedCamera.run_camera)
 
-# thread.start()
+thread.start()
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # connects to the phone's server socket
@@ -78,18 +121,30 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     # initialises the array that will hold the gyroscope data. First element is an array holding x rotational data. Second is y rotational data
     # Third is z rotational data. Fourth holds the time stamps
     dataArray = [[], [], [], [], [], [], []]
+    rotationArray = [[], [], [], [], [], [], []]
 
     plt.ion()  # Turn on interactive mode
     fig, ax = plt.subplots()
 
-    (line1,) = ax.plot([], [], color="red", label="rotation about x")  # First line
-    (line2,) = ax.plot([], [], color="blue", label="rotation about y")  # Second line
-    (line3,) = ax.plot([], [], color="green", label="rotation about z")  # Third line
+    (line1,) = ax.plot([], [], color="red", label="gyro rotation about x")  # First line
+    (line2,) = ax.plot([], [], color="blue", label="gyro rotation about y")  # Second line
+    (line3,) = ax.plot([], [], color="green", label="accel rotation about x")  # Third line
+    (line4,) = ax.plot([], [], color="grey", label="accel rotation about y")  # Third line
 
     ax.set_xlabel("Time")
-    ax.set_ylabel("gyro-data")
-    ax.set_title("real time gyro data")
+    ax.set_ylabel("accel-data")
+    ax.set_title("real time accel data")
     ax.legend()
+    
+    fig1, ax1 = plt.subplots()
+
+    (line11,) = ax1.plot([], [], color="red", label="rotation about x")  # First line
+    (line21,) = ax1.plot([], [], color="blue", label="rotation about y")  # Second line
+
+    ax1.set_xlabel("Time")
+    ax1.set_ylabel("gyro-data")
+    ax1.set_title("real time gyro data")
+    ax1.legend()
 
     while True:
         data = s.recv(1024).decode("utf-8")
@@ -108,24 +163,36 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 counter += 1
 
             if counter == 4:
-                saveJson(jsonCharArr, dataArray)
+                saveJson(jsonCharArr, dataArray, rotationArray)
                 jsonCharArr = ["{"]
                 counter = 1
             else:
                 jsonCharArr.append(char)
 
-        saveJson(jsonCharArr, dataArray)
+        saveJson(jsonCharArr, dataArray, rotationArray)
 
-        line1.set_xdata(dataArray[6])
-        line2.set_xdata(dataArray[6])
-        line3.set_xdata(dataArray[6])
+        line1.set_xdata(rotationArray[6])
+        line2.set_xdata(rotationArray[6])
+        line3.set_xdata(rotationArray[6])
+        line4.set_xdata(rotationArray[6])
 
-        line1.set_ydata(dataArray[3])
-        line2.set_ydata(dataArray[4])
-        line3.set_ydata(dataArray[5])
+        line1.set_ydata(rotationArray[0])
+        line2.set_ydata(rotationArray[1])
+        line3.set_ydata(rotationArray[2])
+        line4.set_ydata(rotationArray[3])
 
         ax.relim()  # Recalculate limits
         ax.autoscale_view()  # Autoscale the view
+        
+        
+        line11.set_xdata(rotationArray[6])
+        line21.set_xdata(rotationArray[6])
+
+        line11.set_ydata(rotationArray[4])
+        line21.set_ydata(rotationArray[5])
+
+        ax1.relim()  # Recalculate limits
+        ax1.autoscale_view()  # Autoscale the view
 
         plt.draw()
         plt.pause(0.01)
--- improvedCamera.py
+++ improvedCamera.py
@@ -9,25 +9,40 @@ import improvedCamera
 from datetime import datetime, timedelta
 from matplotlib.animation import FuncAnimation
 
-rotationAngle = 0
-# xMovement = 0
-# yMovement = 0
+zRot = 0
+xRot = 0
+yRot = 0
 
 
-# def rotateY(rotationValue):
-#     global xMovement
-    
-#     xMovement += rotationValue
-    
-# def rotateX(rotationValue):
-#     global yMovement
-    
-#     yMovement += rotationValue
-    
-# def rotateZ(rotationValue):
-#     global rotationAngle
-    
-#     rotationAngle += rotationValue
+def rotateY(rotationValue):
+    global yRot
+
+    yRot = rotationValue
+
+
+def rotateX(rotationValue):
+    global xRot
+
+    xRot = rotationValue
+
+
+def rotateZ(rotationValue):
+    global zRot
+
+    zRot = rotationValue
+
+
+def getZRot():
+    return zRot
+
+
+def getXRot():
+    return xRot
+
+
+def getYRot():
+    return yRot
+
 
 def rotate_frame(frame, angle, origin):
     (h, w) = frame.shape[:2]
@@ -37,14 +52,6 @@ def rotate_frame(frame, angle, origin):
     return cv2.warpAffine(frame, rotMat, (w, h))
 
 
-def translate_frame(frame, tx, ty):
-    tMatrix = np.float32([[1, 0, tx], [0, 1, ty]])
-    
-    (h, w) = frame.shape[:2]
-
-    return cv2.warpAffine(frame, tMatrix, (w, h))
-
-
 def run_camera():
     cap = cv2.VideoCapture(0)
 
@@ -52,25 +59,29 @@ def run_camera():
     if not cap.isOpened():
         print("Error: Could not access the camera.")
         exit()
-        
-    x, y, width, height = 100, 100, 200, 200
 
+    x, y, width, height = 100, 100, 200, 200
 
     while True:
         # Capture frame-by-frame
         ret, frame = cap.read()
-        
+
         height = min(height, frame.shape[0] - y)
         width = min(width, frame.shape[1] - x)
+
+        global xRot
+        global yRot
+        global zRot
         
-        global rotationAngle
-        
+        x = round(xRot + x)
+        y = round(yRot + y)
+
         # tFrame = translate_frame(frame, xMovement, yMovement)
-        
-        rtFrame = rotate_frame(frame, rotationAngle, (x + width/2, y + height/2))
-        
-        cropped_frame = rtFrame[y:y + height, x:x + width]
-        
+
+        rtFrame = rotate_frame(frame, zRot, (x + width / 2, y + height / 2))
+
+        cropped_frame = rtFrame[y : y + height, x : x + width]
+
         # this is for testing
         if keyboard.is_pressed("right arrow"):
             x += 10
@@ -83,22 +94,22 @@ def run_camera():
 
         if keyboard.is_pressed("down arrow"):
             y += 10
-            
+
         if keyboard.is_pressed("a"):
-            rotationAngle -= 10
+            zRot -= 10
 
         if cv2.waitKey(1) & 0xFF == ord("d"):
-            rotationAngle += 10    
-        
+            zRot += 10
+
         if not ret:
             print("Error: Failed to capture frame.")
             break
-        
-        cv2.imshow('Cropped Camera Feed', cropped_frame)
-        
+
+        cv2.imshow("Cropped Camera Feed", cropped_frame)
+
         # Display the resulting frame
-        cv2.imshow('Camera Feed', frame)
-        
+        cv2.imshow("Camera Feed", frame)
+
         # Exit when the user presses the 'ESC' key
         if cv2.waitKey(1) & 0xFF == 27:  # 27 is the ASCII value for the 'ESC' key
             break
